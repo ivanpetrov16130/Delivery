1840ad06e77a71cb0dccb88a1fa8077d3af44500
Ilya Baykov <ily.boykov@x5.ru>
2022-01-31 15:35:56 +0300

    Initial Commit

---
+++ Delivery/DeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeliveryApp.swift
+//  Delivery
+//
+//  Created by Илья Бойков on 31.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct DeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
